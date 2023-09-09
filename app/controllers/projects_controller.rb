cb7d6fe4404c7d2c9dcba85e77369d6289bc11e3
AlbertoJTD <albertojtd@outlook.com>
2023-09-09 07:45:28 -0600

    Upload and read .mpp files

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,6 +2,7 @@
 
 class ProjectsController < ApplicationController
   before_action :set_project, only: %i[show edit update destroy]
+
   def index
     @projects = Project.all
   end
@@ -12,9 +13,32 @@ class ProjectsController < ApplicationController
 
   def create
     @project = Project.new(project_params)
-
+    # url_for(user.avatar)
+    # url_for(@project.file)
     if @project.save
-      # flash[:success] = 'Object successfully created'
+      require 'active_support'
+      require 'mpxj'
+
+      file_path = ActiveStorage::Blob.service.send(:path_for, @project.file.key)
+
+      project = MPXJ::Reader.read(file_path, nil, :days)
+      # project = MPXJ::Reader.read(@archivo_path, nil, :days)
+      # project = MPXJ::Reader.read('DemoProject.mpp', nil, :days)
+
+      puts "There are #{project.all_tasks.size} tasks in this project"
+      puts "There are #{project.all_resources.size} resources in this project"
+
+      puts "\nThe resources are:"
+      project.all_resources.each do |resource|
+        puts resource.name
+      end
+
+      puts "The tasks are:"
+      project.all_tasks.each do |task|
+        # puts task.properties
+        # break
+        puts "* #{task.name}: starts on #{task.start}, finishes on #{task.finish}, it's duration is #{task.duration}, the percentage completed is: #{task.percent_complete}"
+      end
       redirect_to projects_path, notice: 'Project created'
     else
       render :new, status: :unprocessable_entity
