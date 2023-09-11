f62a4c78da8c13df9243c9f9d0c9b81094cce5c9
AlbertoJTD <albertojtd@outlook.com>
2023-09-11 14:32:45 -0600

    Add parent_id to tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -13,20 +13,27 @@
 #  start_date           :datetime         not null
 #  created_at           :datetime         not null
 #  updated_at           :datetime         not null
+#  parent_id            :bigint
 #  project_id           :bigint           not null
 #
 # Indexes
 #
+#  index_tasks_on_parent_id   (parent_id)
 #  index_tasks_on_project_id  (project_id)
 #
 # Foreign Keys
 #
+#  fk_rails_...  (parent_id => tasks.id)
 #  fk_rails_...  (project_id => projects.id)
 #
 class Task < ApplicationRecord
   belongs_to :project
 
-  has_one :task_dependency
+  has_many :task_dependency
+
+  # Relation between task and itself
+  # belongs_to :parent, class_name: "Task", optional: true
+  # has_many :subtasks, class_name: "Task", foreign_key: "parent_id"
 
   validates :name, presence: true
   validates :start_date, presence: true
