a7d138dadddee6bf9d8eb251cfd38c13564d866b
AlbertoJTD <albertojtd@outlook.com>
2023-09-13 09:35:51 -0600

    Create tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,39 +1,14 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController
-  before_action :set_task, only: %i[edit update show destroy]
-
-  def index
-    project_taks = project.tasks
-    respond_to do |format|
-      format.json { render json: project_taks, status: :ok }
-    end
-  end
-
-  def new
-    @task = Task.new
-  end
-
-  def create
-    if project.tasks.create(task_params)
-      redirect_to project_path(project), notice: 'Task created successfully'
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
-
-  def edit; end
-
-  def show; end
-
-  # def destroy; end
-
-  # API Methods
+  # skip_before_action :verify_authenticity_token, only: :add
   protect_from_forgery
 
   def update
+    task = Task.find(params["id"])
     task.name = params["text"]
     task.start_date = params["start_date"]
+    task.end_date = params["end_date"]
     task.duration = params["duration"]
     task.percentage_completed = params["progress"] || 0
     task.parent_id = params["parent"]
@@ -43,37 +18,27 @@ class TasksController < ApplicationController
   end
 
   def add
-    task_created = Task.create( 
+    @task_created = Task.create( 
       :name => params["text"], 
-      :start_date=> params["start_date"], 
+      :start_date => params["start_date"],
+      :end_date => params["end_date"],
       :duration => params["duration"],
       :percentage_completed => params["progress"], 
-      :parent_id => params["parent"]
+      :parent_id => params["parent"],
+      :project_id => params["project_id"]
     )
-
-    render :json => {:action => "inserted", :tid => task_created.id}
+    pry
+    render :json => {:action => "inserted", :tid => @task_created.id}
   end
 
   def delete
-    task.destroy
+    Task.find(params["id"]).destroy
     render :json => {:action => "deleted"}
   end
 
-  private
+  # private
 
-  def project
-    @project ||= Project.find(params[:project_id])
-  end
-
-  def task
-    @task ||= Task.find(params[:id])
-  end
-
-  def set_task
-    @task = Task.find(params[:id])
-  end
-
-  def task_params
-    params.require(:task).permit(:name, :start_date, :end_date, :completed, :description, :project_id)
-  end
+  # def task
+  #   @task ||= Task.find(params[:id])
+  # end
 end
