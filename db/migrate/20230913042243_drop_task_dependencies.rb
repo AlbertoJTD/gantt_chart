fd530092240c5380946bc51918f1e32cbe62c333
AlbertoJTD <albertojtd@outlook.com>
2023-09-12 22:25:52 -0600

    Drop task_dependencies table

---
+++ db/migrate/20230913042243_drop_task_dependencies.rb
@@ -0,0 +1,16 @@
+class DropTaskDependencies < ActiveRecord::Migration[7.0]
+  def up
+    drop_table :task_dependencies
+  end
+
+  def down
+    create_table :task_dependencies do |t|
+      t.references :task, null: false, foreign_key: true
+      t.references :depends_on_task, null: false, foreign_key: { to_table: :tasks }
+
+      t.timestamps
+    end
+
+    add_index :task_dependencies, %i[task_id depends_on_task_id], unique: true
+  end
+end
