b1100333d3e1a287797945af3c9c7abd2b4b4313
AlbertoJTD <albertojtd@outlook.com>
2023-09-13 10:00:12 -0600

    Fix bug to create and update tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController
-  # skip_before_action :verify_authenticity_token, only: :add
   protect_from_forgery
 
   def update
@@ -11,34 +10,28 @@ class TasksController < ApplicationController
     task.end_date = params["end_date"]
     task.duration = params["duration"]
     task.percentage_completed = params["progress"] || 0
-    task.parent_id = params["parent"]
     task.save
 
     render :json => {:action => "updated"}
   end
 
   def add
-    @task_created = Task.create( 
+    parent_id = params["parent"].to_i.positive? ? params["parent"] : nil
+    task_created = Task.create( 
       :name => params["text"], 
       :start_date => params["start_date"],
       :end_date => params["end_date"],
       :duration => params["duration"],
       :percentage_completed => params["progress"], 
-      :parent_id => params["parent"],
+      :parent_id => parent_id,
       :project_id => params["project_id"]
     )
-    pry
-    render :json => {:action => "inserted", :tid => @task_created.id}
+
+    render :json => {:action => "inserted", :tid => task_created.id}
   end
 
   def delete
-    Task.find(params["id"]).destroy
+    Task.find(params[:id]).destroy
     render :json => {:action => "deleted"}
   end
-
-  # private
-
-  # def task
-  #   @task ||= Task.find(params[:id])
-  # end
 end
